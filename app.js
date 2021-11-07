7a42626e4ec8915b7efc57a8e74e2c8b14a702b3
Pedro leite <pedromiguelgoncalvesleite@gmail.com>
2021-11-07 22:05:25 +0000

    more work

--- app.js
+++ app.js
@@ -1,41 +1,64 @@
 #!usr/bin/node
 const yargs = require("yargs");
-
-const text = 'I would really like to drive your car around the block a few times.';
-const lang = 'it';
-
-(async () =>{
-  const getTranslation = require("./api");
-  var test = await getTranslation.translateText(text, lang).then((response) => console.log(response));
-})();
+// (async () =>{
+//   const getTranslation = require("./api");
+//   var test = await getTranslation.translateText(text, lang).then((response) => console.log(response));
+// })();
 
 const options = yargs
-  .usage("Usage: -w <word> -l <language>")
-  .option("w", {
-    alias: "word",
-    describe: "Word to be translated",
+  .usage("Usage: -t <text>")
+  .option("t", {
+    alias: "text",
+    describe: "text to be translated",
     type: "string",
     demandOption: false,
   })
-  .usage("Usage: -n <newspaper>")
-  .option("mw", {
-    alias: "multiple words",
-    describe:
-      "Multiple words to be translated",
-    type: "number",
-    demandOption: false,
-  })
-  .usage("Usage: -c <cripto symbol>")
-  .option("c", {
-    alias: "symbol",
-    describe: "CriptoCurrency Symbol",
+  .usage("Usage: -l <language>")
+  .option("l", {
+    alias: "language",
+    describe: "language",
     type: "string",
     demandOption: false,
   })
-  .usage("Usage: -c <cripto symbol>")
-  .option("c", {
-    alias: "symbol",
-    describe: "CriptoCurrency Symbol",
+  .usage("Usage: -d <detect>")
+  .option("d", {
+    alias: "detect",
+    describe: "detect language",
     type: "string",
     demandOption: false,
-  }).argv;
+  }
+  ).argv;
+
+  //Always has to be the text argument first
+  const firstArg = process.argv[2];
+  const text = process.argv[3];
+
+  //Always has to come after, can be -l or -d
+  const secondArg = process.argv[4];
+  
+  if(secondArg == '-l'){
+    const language = process.argv[5].split(',');
+    console.log('language :', language);
+  }
+
+
+  if(firstArg == "-w"){
+    (async () =>{
+    const getTranslation = require("./controllers/api");
+    const result = await getTranslation.translateText(text, language).then((response) => console.log(response));
+    })();
+
+  }else if(firstArg == "-s"){
+
+  }else if(firstArg == "-d"){
+
+  }  else {
+  console.log(
+    "\n***************************************" +
+      "\n*  Welcome to Translator CLI app!   *" +
+      "\n* -w  <word>                        *" +
+      "\n* -s  <sentence> (for help, --help) *" +
+      "\n* -l  <language>                    *" +
+      "\n*************************************"
+      );
+  }
